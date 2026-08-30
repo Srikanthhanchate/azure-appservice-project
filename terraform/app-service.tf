9d8203248cd9fe03d4653f66d81b8d471c9bffb0
Srikanthhanchate <Srikanthhanchate6@gmail.com>
2026-08-30 22:53:32 +0530

    Update infrastructure configuration

--- terraform/app-service.tf
+++ terraform/app-service.tf
@@ -34,24 +34,22 @@ resource "azurerm_linux_web_app" "app" {
   }
 
   site_config {
-  always_on = true
+    always_on = true
 
-  application_stack {
-    java_version        = "21"
-    java_server         = "JAVA"
-    java_server_version = "21"
+    application_stack {
+      java_version        = "21"
+      java_server         = "JAVA"
+      java_server_version = "21"
+    }
   }
-}
 
   virtual_network_subnet_id = azurerm_subnet.app_service_integration.id
 
   app_settings = {
-  AZURE_SQL_CONNECTION_STRING = "jdbc:sqlserver://${azurerm_mssql_server.main.fully_qualified_domain_name}:1433;databaseName=${azurerm_mssql_database.main.name};msiClientId=${azurerm_user_assigned_identity.app.client_id};authentication=ActiveDirectoryManagedIdentity;encrypt=true;trustServerCertificate=false;"
-
-  JWT_SECRET = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.jwt_secret.versionless_id})"
-
-  APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.app.connection_string
-}
+    AZURE_SQL_CONNECTION_STRING = "jdbc:sqlserver://${azurerm_mssql_server.main.fully_qualified_domain_name}:1433;databaseName=${azurerm_mssql_database.main.name};msiClientId=${azurerm_user_assigned_identity.app.client_id};authentication=ActiveDirectoryMSI;"
+    JWT_SECRET                  = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.jwt_secret.versionless_id})"
+    APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.app.connection_string
+  }
 
   tags = local.common_tags
 }
