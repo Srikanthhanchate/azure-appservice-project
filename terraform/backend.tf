34a9d48a91f3785a4c88eaf9a563439b13563ba2
Srikanthhanchate <Srikanthhanchate6@gmail.com>
2026-08-30 17:51:38 +0530

    Configure Terraform backend OIDC

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,5 +7,8 @@ terraform {
 
     use_oidc         = true
     use_azuread_auth = true
+
+    tenant_id       = "6c794cbc-3892-48c4-a876-3001cc64351a"
+    client_id       = "c6a0888e-1895-40f8-a14d-5d3d6ef2ca1b"
   }
 }
